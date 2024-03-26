ef7ab57b6704afeec217b70b23022238e4c0ba77
amiroooamiran <amiran.amirhossein@gmail.com>
2024-03-27 02:31:22 +0330

    📎 Function ChaCha get Random Generator

--- src/gen_keys.rs
+++ src/gen_keys.rs
@@ -8,3 +8,10 @@ use p256::SecretKey as PrivateKey;
 use rand_chacha::rand_core::{RngCore, SeedableRng};
 use rand_chacha::ChaChaRng;
 
+pub fn get_randomizer(seed: Option<[u8; 32]>) -> ChaChaRng {
+    if let Some(seed) = seed{
+        ChaChaRng::from_seed(seed)
+    } else {
+        ChaChaRng::from_entropy()
+    }
+}
