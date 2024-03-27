5b4035455164f8acd845713b783847fb363da62b
amiroooamiran <amiran.amirhossein@gmail.com>
2024-03-27 05:51:37 +0330

    🔗 Use dependencies in main file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod gen_keys;
+
+use chacha20poly1305::aead::Aead;
+use chacha20poly1305::ChaCha20Poly1305;
+use chacha20poly1305::KeyInit;
+use chacha20poly1305::Nonce;
+
+use clap::Parser;
+
+
 fn main() {
     println!("Hello, world!");
 }
