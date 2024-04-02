75a6c3531d55479142558158eaff0076cf6b3c52
amiroooamiran <amiran.amirhossein@gmail.com>
2024-04-03 02:34:55 +0330

    📎 Function Reads the private key and public key

--- src/gen_keys.rs
+++ src/gen_keys.rs
@@ -1,27 +1,36 @@
 use std::fs::File;
 use std::io::{self, Write};
+use std::io::stdout;
 
 use chacha20::Key;
 use hkdf::Hkdf;
+
 use p256::ecdh::diffie_hellman;
 use p256::elliptic_curve::zeroize::Zeroize;
-use p256::pkcs8::{DecodePrivateKey, DecodePublicKey,EncodePrivateKey, EncodePublicKey};
+
+use p256::pkcs8::DecodePrivateKey;
+use p256::pkcs8::DecodePublicKey;
+use p256::pkcs8::EncodePrivateKey;
+use p256::pkcs8::EncodePublicKey;
+
 use p256::PublicKey;
 use p256::SecretKey as PrivateKey;
+
 use rand_chacha::rand_core::{RngCore, SeedableRng};
+
 use rand_chacha::ChaChaRng;
 use sha2::digest::Output;
 
-// 
+// Generate random keys
 pub fn get_randomizer(seed: Option<[u8; 32]>) -> ChaChaRng {
-    if let Some(seed) = seed{
+    if let Some(seed) = seed {
         ChaChaRng::from_seed(seed)
     } else {
         ChaChaRng::from_entropy()
     }
 }
 
-// Generate a public and private key 
+// Generate a public and private key
 pub fn get_keypair(seed: Option<[u8; 32]>) -> (PrivateKey, PublicKey) {
     let mut rng = get_randomizer(seed);
 
@@ -31,19 +40,33 @@ pub fn get_keypair(seed: Option<[u8; 32]>) -> (PrivateKey, PublicKey) {
     (private_key, public_key)
 }
 
-// convart Bytes of the keys to pem format 
-pub fn dump_asym_keys(priv_key: &PrivateKey, pub_key: &PublicKey, out: Option<&str>) -> io::Result<()> {
+// convart Bytes of the keys to pem format
+pub fn dump_asym_keys(
+    priv_key: &PrivateKey,
+    pub_key: &PublicKey,
+    out: Option<&str>,
+) -> io::Result<()> {
     let priv_pem_result = priv_key.to_pkcs8_pem(Default::default());
     let pub_pem_result = pub_key.to_public_key_pem(Default::default());
 
     let priv_pem = match priv_pem_result {
         Ok(pem) => pem,
-        Err(err) => return Err(io::Error::new(io::ErrorKind::Other, format!("Failed to convert private key to PEM: {}", err))),
+        Err(err) => {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                format!("Failed to convert private key to PEM: {}", err),
+            ))
+        }
     };
 
     let pub_pem = match pub_pem_result {
         Ok(pem) => pem,
-        Err(err) => return Err(io::Error::new(io::ErrorKind::Other, format!("Failed to convert public key to PEM: {}", err))),
+        Err(err) => {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                format!("Failed to convert public key to PEM: {}", err),
+            ))
+        }
     };
 
     let (mut priv_writer, mut pub_writer): (Box<dyn Write>, Box<dyn Write>) = match out {
@@ -56,14 +79,18 @@ pub fn dump_asym_keys(priv_key: &PrivateKey, pub_key: &PublicKey, out: Option<&s
                 Box::new(File::create(&pub_file)?),
             )
         }
-        None => (
-            Box::new(io::stdout()),
-            Box::new(io::stdout())
-        )
+        None => (Box::new(io::stdout()), Box::new(io::stdout())),
     };
 
     priv_writer.write_all(priv_pem.as_bytes())?;
     pub_writer.write_all(pub_pem.as_bytes())?;
 
     Ok(())
+}
+
+// Reads the private key from `priv_file` and the public key from `pub_file`
+pub fn import_asym_keys(priv_file: &str, pub_file: &str) -> (PrivateKey, PublicKey) {
+    let pub_key = PublicKey::read_public_key_pem_file(pub_file).unwrap();
+    let priv_key = PrivateKey::read_pkcs8_pem_file(priv_file).unwrap();
+    (priv_key, pub_key)
 }
